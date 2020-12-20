8a552accc1c856fd26022744157dd02614c56d2e
Henrique.Gouveia-ext <Henrique.Gouveia-ext@ab-Inbev.com>
2020-12-20 12:32:43 -0300

    Allow all ip range to connect to it

--- config/config.js
+++ config/config.js
@@ -9,15 +9,15 @@
  */
 
 var config = {
-	address: "0.0.0.0", 	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
+	address: "0.0.0.0",		 	// Address to listen on, can be:
+								// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+								// - another specific IPv4/6 to listen on a specific interface
+								// - "0.0.0.0", "::" to listen on any interface
+								// Default, when address config is left out or empty, is "localhost"
 	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
 					// you must set the sub path here. basePath must end with a /
-	ipipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.0.100", "::ffff:192.168.0.120"], 	// Set [] to allow all IP addresses
+	ipWhitelist: [], 	// Set [] to allow all IP addresses
 																												// or add a specific IPv4 of 192.168.1.5 :
 																												// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 																												// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -140,7 +140,7 @@ var config = {
 		    config: {
 		        customCommand: {},  // Optional, See "Using Custom Commands" below
 		        showModuleApiMenu: true, // Optional, Enable the Module Controls menu
-		        secureEndpoints: true, // Optional, See API/README.md
+		        secureEndpoints: false, // Optional, See API/README.md
 		        // uncomment any of the lines below if you're gonna use it
 		        // customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
 		        // apiKey: "", // Optional, See API/README.md for details
